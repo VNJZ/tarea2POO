e2082830c19eac32ccf28389f413eed9c360430b
joaquinchinobelmar <chinobelmar@gmai.com>
2025-05-30 04:52:43 -0400

    Etapa 5.2 lista con logica reestructurada

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module cl.utfsm.tarea2.tarea2poo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens cl.utfsm.tarea2.tarea2poo to javafx.fxml;
-    exports cl.utfsm.tarea2.tarea2poo;
-}
